b41d2696880fef5f45c083a94870352ae01b861b
Galina-Petrovna <148689038+Galina-Petrovna@users.noreply.github.com>
2024-04-14 00:15:22 +0300

    navigation

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Catalog } from "./components/Catalog/Catalog.jsx";
+import { Header } from "./components/Header/Header.jsx";
+import { Navigation } from "./components/Navigation/Navigation.jsx";
+
+export const App = () => {
+
+  return (
+  <>
+    <Header />
+    <main>
+      <Navigation />
+      <Catalog />
+    </main>
+    <footer></footer>
+  </>
+  )
+}
+
+
